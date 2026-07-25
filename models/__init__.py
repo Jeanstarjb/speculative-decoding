b8b50a849012c635715995b5ae02cf3d45fb7d77
Jeff Munyaka <mogakaj96@gmail.com>
2026-07-25 22:04:49 +0300

    chore(core): initialize model architecture stubs

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+# Model architecture definitions
+
+class DraftModel:
+    """Lightweight draft model for speculative decoding"""
+    pass
+
+
+class TargetModel:
+    """Main language model for final inference"""
+    pass
